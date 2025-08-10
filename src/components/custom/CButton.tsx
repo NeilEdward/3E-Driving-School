6e509b8869df65c2cebe8c451fb141ed5a5b0aaa
NeilEdward <89566383+NeilEdward@users.noreply.github.com>
2025-08-10 09:47:06 +0800

    styles:button, radius -- .65rem

--- src/components/custom/CButton.tsx
+++ src/components/custom/CButton.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { Button } from "../ui/button";
+import {Button} from "../ui/button";
 
 type CButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   label: string;
 };
 
-const CButton = ({ label, ...props }: CButtonProps) => {
+const CButton = ({label, ...props}: CButtonProps) => {
   return (
-    <Button
-      className="text-amber-400 hover:bg-amber-500 hover:text-black cursor-pointer active:bg-amber-600"
-      {...props}
-    >
+    <Button className="cursor-pointer" {...props}>
       {label}
     </Button>
   );
